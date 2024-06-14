84b9ad2edb8830c841545a5937cc187daa38a9e7
Sonic [Lucas Maurice] <sonic@djls.io>
2024-06-14 00:02:00 -0400

    Implement Grouping by region #7

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+import typer
+from typing_extensions import Annotated
 from typing import Optional
 
-import typer
 from tabulate import tabulate
 
-from s3_lister import get_bucket, list_buckets
+from s3_lister import get_bucket, list_buckets, available_grouping
 
 
 def print_as_table(bucket_information):
     print(tabulate(bucket_information, headers="keys"))
 
 
-def main(bucket: Optional[str] = typer.Argument(None)):
+def main(
+    bucket: Annotated[str, typer.Option(help="The bucket name to filter. Should be the entire bucket name.")] = "",
+    group_by: Annotated[Optional[str], typer.Option(help="this option does this and that", callback=available_grouping)] = "",
+):
     if bucket:
         if bucket_information := get_bucket(bucket):
             print_as_table([bucket_information])
-    else:
-        print_as_table(list_buckets())
+        return
+
+    buckets_data = list_buckets(group_by)
+
+    if group_by:
+        print(f"Grouping by {group_by}")
+
+    for group, data in buckets_data.items():
+        if group_by:
+            print(f"Data for {group_by} {group}:\n-----------")
+        print_as_table(data["buckets"])
+        print("-----------")
+        print(f"Summary:\tBuckets: {data['buckets_count']}, Files: {data['files_count']}, Cost: {data['total_cost']}, Data: {data['total_size']} bytes")
+        print("\n")
 
 
 if __name__ == "__main__":
--- s3_lister.py
+++ s3_lister.py
@@ -8,24 +8,35 @@ from mypy_boto3_s3 import S3Client
 COST_PER_GB = 0.023
 COST_PER_BYTE = COST_PER_GB / 1024 / 1024 / 1024
 
+AVAILABLE_GROUPS = ["", "region"]
+
+
+def available_grouping(value: str):
+    if value not in AVAILABLE_GROUPS:
+        raise ValueError(f"Invalid grouping selected `{value}`. Must be one of {AVAILABLE_GROUPS}")
+    return value
+
 
 def get_bucket(name):
     print(f"Getting bucket with name {name}")
 
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(name)
-
     client = boto3.client("s3")
 
     last_modified = datetime.min
     last_modified = last_modified.replace(tzinfo=tzutc())
     bucket_object_count = 0
     bucket_size = 0
+    bucket_location = ""
 
     paginator = client.get_paginator('list_objects_v2')
-    for objects in paginator.paginate(Bucket=bucket.name):
+    for objects in paginator.paginate(Bucket=name):
         if not (contents := objects.get("Contents")):
             continue
+
+        bucket_location = objects["ResponseMetadata"]["HTTPHeaders"]["x-amz-bucket-region"]
+
         for bucket_object in contents:
             if (new_last_modified := bucket_object["LastModified"]) > last_modified:
                 last_modified = new_last_modified
@@ -41,17 +52,45 @@ def get_bucket(name):
         nb_files=bucket_object_count,
         total_size_bytes=bucket_size,
         last_modified=last_modified,
+        region=bucket_location,
         cost=round(COST_PER_BYTE * bucket_size, 2),
     )
 
 
-def list_buckets():
-    client: S3Client = boto3.client("s3")
+def list_buckets(group_by: str = ""):
+    client = boto3.client("s3")
 
-    all_buckets_info = []
+    all_buckets_info = {}
+
     for bucket in client.list_buckets()["Buckets"]:
-        print(f'Getting bucket info for {bucket["Name"]}')
-        if bucket_info := get_bucket(bucket["Name"]):
-            all_buckets_info.append(bucket_info)
+        bucket_info = get_bucket(bucket["Name"])
+
+        # Ignore empty buckets
+        if not bucket_info:
+            continue
+
+        # Use the group_by tag to group the buckets - or use "all" if no grouping is selected
+        current_group = "all"
+        if group_by:
+            current_group = bucket_info[group_by]
+
+        # If the grouping tag is not in the dictionary, add it
+        if current_group not in all_buckets_info:
+            all_buckets_info[current_group] = {
+                "total_cost": 0,
+                "total_size": 0,
+                "files_count": 0,
+                "buckets_count": 0,
+                "buckets": [],
+            }
+
+        # Update the summary
+        all_buckets_info[current_group]["total_cost"] += bucket_info["cost"]
+        all_buckets_info[current_group]["total_size"] += bucket_info["total_size_bytes"]
+        all_buckets_info[current_group]["files_count"] += bucket_info["nb_files"]
+        all_buckets_info[current_group]["buckets_count"] += 1
+
+        # Insert the bucket information
+        all_buckets_info[current_group]["buckets"].append(bucket_info)
 
     return all_buckets_info
